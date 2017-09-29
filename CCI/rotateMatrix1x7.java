b3748c90cf27c8365204c0a1cc1ac6000adeef38
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-09-29 18:41:26 -0400

    Added question 1.7 from CCI, but without an InPlace Method

---
+++ CCI/rotateMatrix1x7.java
@@ -0,0 +1,32 @@
+package CCI;
+
+public class rotateMatrix1x7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int matrixSize = 3;
+		int[][] matrix = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+		int[][] rotatedMatrix = rotateMatrix(matrix, matrixSize);
+		
+		for(int iterator = 0; iterator < matrixSize; iterator++) {
+			for(int subIter = 0; subIter < matrixSize; subIter++) {
+				System.out.print(rotatedMatrix[iterator][subIter]);
+			}
+			System.out.println("");
+		}
+		
+	}
+	//Not in place method
+	public static int[][] rotateMatrix(int[][] matrix, int matrixSize){
+		int[][] tempMatrix = {new int[matrixSize],new int[matrixSize],new int[matrixSize]};
+		
+		for(int iterator = 0; iterator < matrixSize; iterator++) {
+			for(int subIter = 0; subIter < matrixSize; subIter++) {
+				tempMatrix[iterator][subIter] = matrix[2 - subIter][iterator];
+			}
+		}
+		
+		return tempMatrix;
+	}
+
+}
